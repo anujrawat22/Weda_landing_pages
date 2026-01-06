7da663e8490e68bb09e35eacc5f2ccb8a0a633a2
anuj.rawat <anuj@myhealthcare.life>
2026-01-06 23:00:36 +0530

    ss rms changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import AllInOneClass6 from './pages/allInOneClass6'
 
 function App() {
- 
+
   return (
     <>
-        <Routes>
-      <Route path="/" element={<AllInOneClass6 />} />
-     </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/ss-rms-class-6" replace />} />
+        <Route path="/ss-rms-class-6" element={<AllInOneClass6 />} />
+      </Routes>
     </>
   )
 }
